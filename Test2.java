fdb6566869298dd2d9e22040874584b22a2c44b5
AyeshaNilofer <zaidson@gmail.com>
2026-07-16 12:23:19 +0530

    New files

---
+++ Test2.java
@@ -0,0 +1,12 @@
+public class Test2 {
+
+    
+    public static void main(String umar[])
+    {
+   
+         System.out.print("java2");
+
+    }
+
+    
+}
---
+++ Test3.java
@@ -0,0 +1,12 @@
+public class Test3 {
+
+    
+    public static void main(String umar[])
+    {
+   
+         System.out.print("java3");
+
+    }
+
+    
+}
